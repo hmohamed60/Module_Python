7ceb8eb005f232e393843baee3c74c7629b30730
hmohamed60 <148979877+hmohamed60@users.noreply.github.com>
2023-10-27 15:24:00 +0200

    Delete Projet_1 directory

--- Projet_1/main.py
+++
@@ -1,6 +0,0 @@
-from qcm import QCM
-from questions import questions
-
-if __name__ == "__main__":
-    qcm = QCM(questions)
-    qcm.start_quiz()
--- Projet_1/reponse.py
+++
@@ -1,4 +0,0 @@
-class Reponse:
-    def __init__(self, question, user_answer):
-        self.question = question
-        self.user_answer = user_answer
